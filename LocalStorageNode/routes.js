022b33345ce4a49cb269e7b0c1383a312f6a75d3
WallisonLima <wallisonlimaoliveira@gmail.com>
2021-06-08 19:05:30 -0300

    Atualizaçao do projeto

--- LocalStorageNode/routes.js
+++ LocalStorageNode/routes.js
@@ -8,46 +8,46 @@ const db = require('./BD/database')
 routes.get('/', async(req, res)=>{
     let products = await db.findDB('Products')
     let contentProduct = await GetProducts(products)
-    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
-    let body = await helpers.GetPart(__dirname + "/public_html/views/index.html", [{search: '{{Produtos}}', replace: contentProduct}])
-    let content = headr + body
+    let content = '';
+    content += await helpers.GetPart(__dirname + "/public_html/views/headr.html")
+    content += await helpers.GetPart(__dirname + "/public_html/views/index.html", [{search: '{{Produtos}}', replace: contentProduct}])
     res.send(content)
 })
 
 routes.get('/pedidos', async (req, res)=>{
-    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
-    let body = await helpers.GetPart(__dirname + "/public_html/views/pedidos.html")
-    let content = headr + body
+    let content = '';
+    content += await helpers.GetPart(__dirname + "/public_html/views/headr.html")
+    content += await helpers.GetPart(__dirname + "/public_html/views/pedidos.html")
     res.send(content)
 })
 
 routes.get('/carrinho', async (req, res)=>{
-    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
-    let body = await helpers.GetPart(__dirname + "/public_html/views/carrinho.html")
-    let content = headr + body
+    let content = ''
+    content += await helpers.GetPart(__dirname + "/public_html/views/headr.html")
+    content += await helpers.GetPart(__dirname + "/public_html/views/carrinho.html")
     res.send(content)
 })
 
 routes.get('/cadastro', async (req, res)=>{
-    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
-    let body = await helpers.GetPart(__dirname + "/public_html/views/cadastroPessoa.html")
-    let content = headr + body
+    let content = '';
+    content += await helpers.GetPart(__dirname + "/public_html/views/headr.html")
+    content += await helpers.GetPart(__dirname + "/public_html/views/cadastroPessoa.html")
     res.send(content)
 })
 
 routes.get("/cadastrar-produto", async (req, res)=>{
-    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
-    let body = await helpers.GetPart(__dirname + "/public_html/views/cadastroProdutos.html")
-    let content = headr + body
+    let content = '';
+    content += await helpers.GetPart(__dirname + "/public_html/views/headr.html")
+    content += await helpers.GetPart(__dirname + "/public_html/views/cadastroProdutos.html")
     res.send(content)
 })
 
 
 routes.post('/cadastro/pedido', async (req, res)=>{
     let resp = await insertDB('Orders', body);
-    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
-    let body = await helpers.GetPart(__dirname + "/public_html/views/pedidos.html")
-    let content = headr + body
+    let content = '';
+    content += await helpers.GetPart(__dirname + "/public_html/views/headr.html")
+    content += await helpers.GetPart(__dirname + "/public_html/views/pedidos.html")
     res.send(content)
 })
 
