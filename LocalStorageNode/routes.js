7edbe067914d5270698784a156c820eae5365bb3
WallisonLima <wallisonlimaoliveira@gmail.com>
2021-06-08 17:22:50 -0300

    Atualizaçao do projeto, implementando header dinamico

--- LocalStorageNode/routes.js
+++ LocalStorageNode/routes.js
@@ -7,36 +7,48 @@ const db = require('./BD/database')
 
 routes.get('/', async(req, res)=>{
     let products = await db.findDB('Products')
-    let content = await GetProducts(products)
-    let body = await helpers.GetPart(__dirname + "/public_html/views/index.html", [{search: '{{Produtos}}', replace: content}])
-    res.send(body)
+    let contentProduct = await GetProducts(products)
+    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
+    let body = await helpers.GetPart(__dirname + "/public_html/views/index.html", [{search: '{{Produtos}}', replace: contentProduct}])
+    let content = headr + body
+    res.send(content)
 })
 
 routes.get('/pedidos', async (req, res)=>{
+    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
     let body = await helpers.GetPart(__dirname + "/public_html/views/pedidos.html")
-    res.send(body)
+    let content = headr + body
+    res.send(content)
 })
 
 routes.get('/carrinho', async (req, res)=>{
+    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
     let body = await helpers.GetPart(__dirname + "/public_html/views/carrinho.html")
-    res.send(body)
+    let content = headr + body
+    res.send(content)
 })
 
 routes.get('/cadastro', async (req, res)=>{
+    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
     let body = await helpers.GetPart(__dirname + "/public_html/views/cadastro.html")
-    res.send(body)
+    let content = headr + body
+    res.send(content)
 })
 
 routes.get("/cadastrar-produto", async (req, res)=>{
+    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
     let body = await helpers.GetPart(__dirname + "/public_html/views/produtos.html")
-    res.send(body)
+    let content = headr + body
+    res.send(content)
 })
 
 
 routes.post('/cadastro/pedido', async (req, res)=>{
     let resp = await insertDB('Orders', body);
+    let headr = await helpers.GetPart(__dirname + "/public_html/views/headr.html")
     let body = await helpers.GetPart(__dirname + "/public_html/views/pedidos.html")
-    res.send(body)
+    let content = headr + body
+    res.send(content)
 })
 
 routes.post("/cadastrar-produto/cadastrar", async(req, res)=>{
